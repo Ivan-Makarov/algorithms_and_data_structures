856df805e533057b8de3e7d243804dcd5b833922
Ivan-Makarov <ivan.ma@remoteassembly.com>
2019-05-22 12:33:07 +0300

    add solution for lesson 1 task 9

---
+++ Lesson_1/Les_1_task_9.py
@@ -0,0 +1,18 @@
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+
+if a > b:
+    if a < c:
+        print(a)
+    elif c > b:
+        print(c)
+    else:
+        print(b)
+else:
+    if b < c:
+        print(b)
+    elif c < a:
+        print(a)
+    else:
+        print(c)
